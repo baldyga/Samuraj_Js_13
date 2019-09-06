7e3fe0962be8d13a3c245cc239bac00e9285d399
Aleksandra <50842332+baldyga@users.noreply.github.com>
2019-09-06 10:21:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const chars = 'ABCDEFGHIJKLMNOP123456789';
+
+const btn = document.querySelector('button');
+const section = document.querySelector('section');
+
+const codesNumber = 1000;
+const charsNumber = 10;
+
+const codesGenerator = () => {
+    for(let i = 0; i < codesNumber; i++) {
+        let code = "";
+
+        for (let i = 0; i < charsNumber; i++) {
+            const index = Math.floor(Math.random() * 20);
+            // console.log(index);
+            code += chars[index];
+        }
+        const div = document.createElement('div');
+        div.textContent = code;
+        section.appendChild(div);
+    }
+}
+btn.addEventListener('click', codesGenerator)
